acd780806b5e3d1defcf69019943f77f409bab59
Pavlova <irochkapavlova85@gmail.com>
2019-11-29 13:48:45 +0200

    edits

--- src/index.js
+++ src/index.js
@@ -1,29 +1,32 @@
-'use strict';
+"use strict";
 
-import './css/styles.css';
-import {refs} from './services/refs.js';
-import fetch from './components/fetchCountries';
-import country from './templates/country.hbs';
-import list from './templates/list.hbs';
-import PNotify from '../node_modules/pnotify/dist/es/PNotify.js';
-import '../node_modules/pnotify/dist/PNotifyBrightTheme.css';
-import debounce from 'lodash.debounce';
-
+import "./css/styles.css";
+import { refs } from "./services/refs.js";
+import fetch from "./components/fetchCountries";
+import country from "./templates/country.hbs";
+import list from "./templates/list.hbs";
+import PNotify from "../node_modules/pnotify/dist/es/PNotify.js";
+import "../node_modules/pnotify/dist/PNotifyBrightTheme.css";
+import debounce from "lodash.debounce";
 
 refs.input.addEventListener(
-  'input',
+  "input",
   debounce(event => {
-    fetch.fetchCountries(event.target.value)
+    if (event.target.value === "") {
+      return;
+    }
+    fetch
+      .fetchCountries(event.target.value)
       .then(data => {
         if (data.length === 1) {
           refs.countries.innerHTML = `${country(data)}`;
         } else if (data.length >= 2 && data.length <= 10) {
           refs.countries.innerHTML = `${list(data)}`;
-        } else if(data.length > 10)
+        } else if (data.length > 10)
           PNotify.error(
-            'Too many matches found. Please, enter a more specific query!',
+            "Too many matches found. Please, enter a more specific query!"
           );
       })
-      .catch(error => console.log(error))
-  }, 1000),
+      .catch(error => PNotify.error(error));
+  }, 1000)
 );
